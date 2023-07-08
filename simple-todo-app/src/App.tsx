45495afaeb2fcc1a9a2515b95e0a4d2aaffa4ae1
yuni-0928 <yuniyuni0928@gmail.com>
2023-07-09 00:25:33 +0900

    フォームを追加しました

--- simple-todo-app/src/App.tsx
+++ simple-todo-app/src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <form>
+        <label>
+          タスク：
+          <input type="text" name="task" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
 }
 
 export default App;
